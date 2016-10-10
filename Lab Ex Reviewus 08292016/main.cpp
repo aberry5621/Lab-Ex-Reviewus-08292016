6314e57d94e03be5a3ba8a0f3f3d67f6dc4ed4cd
alexberry721 <alexberry721@gmail.com>
2016-10-10 10:00:24 -0700

    Initial Commit

---
+++ Lab Ex Reviewus 08292016/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab Ex Reviewus 08292016
+//
+//  Created by ax on 10/10/16.
+//  Copyright © 2016 COMP130. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
